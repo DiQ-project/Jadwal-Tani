9c4d70aa0728a8344776c5d0108beb6262ff1530
DiQ-project <muhammad10sidik@gmail.com>
2026-09-08 17:03:17 +0700

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jadwaltani.app',
+  appName: 'Jadwal Tani',
+  webDir: '.'
+};
+
+export default config;
