bdf1c2fe3caa1f1cf1f6172d208f721ebdc03634
DiQ-project <muhammad10sidik@gmail.com>
2026-09-09 07:45:53 +0700

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.jadwaltani.app',
   appName: 'Jadwal Tani',
-  webDir: 'www'
+  webDir: '.',
+  plugins: {
+    LocalNotifications: {
+      smallIcon: 'ic_launcher',
+      iconColor: '#2f7d32'
+    }
+  }
 };
 
 export default config;
